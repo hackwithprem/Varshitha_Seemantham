32ae50982f6dd24527f1ea66e1016fe98bda0f00
hackwithprem <justconnet2prem@gmail.com>
2026-05-22 15:11:20 +0530

    Fixed runtime crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
 import "./styles.css";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+});
 
-const rootElement = document.getElementById("root")!;
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
